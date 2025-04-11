96c01d9c21cae1acb7c70b34beff907dc5eb5727
lmyudowitz <lmyudowitz@gmail.com>
2025-04-11 13:36:22 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -285,3 +285,18 @@ document.addEventListener('DOMContentLoaded', _ => {
   const randImageIndex = ~~(Math.random() * images.length);
   document.getElementById('randImg').src = images[randImageIndex];
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 9000); // Change image every 5 seconds
+}
